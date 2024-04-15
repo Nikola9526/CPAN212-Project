1d96ba27d152e9ae59935b8a9e4c193c5c089626
Niki9526 <n01591561@humber.ca>
2024-04-15 14:34:23 -0400

    added file layout tempalte

---
+++ react/recipeapp/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+// rfc tab
+export default function Footer() {
+  return (
+    <footer className="bg-dark text-white mt-5 p-4 text-center">
+    Copyright &copy; {new Date().getFullYear()} GoodFood
+  </footer>
+  )
+}
